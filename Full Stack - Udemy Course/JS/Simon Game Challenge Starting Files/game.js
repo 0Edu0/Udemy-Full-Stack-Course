96f6e0f9a64a7a70bb1aa8d4743959f3c2532815
Eduardo <edulink.1610@gmail.com>
2025-11-18 21:33:23 -0600

    Add sequence comparator and click funcion

--- Full Stack - Udemy Course/JS/Simon Game Challenge Starting Files/game.js
+++ Full Stack - Udemy Course/JS/Simon Game Challenge Starting Files/game.js
@@ -1,6 +1,28 @@
 let buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
+let start = false;
+let level = 0;
+
+//Compare both patterns function
+function checkAnswer(currentLevel){
+    if(currentLevel===gamePattern.length-1){
+        console.log("success");
+    } else {
+        console.log("wrong");
+    }
+};
+
+//Key pressed function
+$(document).keypress(function() {
+    if (start !== true) {
+        $('h1').text("Level"+" "+level);
+        nextSequence();
+        start = true;
+    };
+});
+
+
 
 //Click function
 $(".btn").click(function() {
@@ -8,6 +30,9 @@ $(".btn").click(function() {
     userClickedPattern.push(userChosenColor);
     playSound(userChosenColor);
     animatePress(userChosenColor);
+    checkAnswer(userClickedPattern-1);
+    console.log(userClickedPattern.length-1);
+    console.log(gamePattern.length-1);
 });
 
 //Sequence function
@@ -17,6 +42,7 @@ function nextSequence() {
     gamePattern.push(randomChosenColor);
     $("#"+randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColor);
+    level++;
 }
 
 //Sounds function
