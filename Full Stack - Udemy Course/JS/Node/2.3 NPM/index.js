bc9acb1835cb6ecc435997ec21fadd6b792a1327
Eduardo <edulink.1610@gmail.com>
2025-12-01 21:11:41 -0600

    Complete NPM exercise

---
+++ Full Stack - Udemy Course/JS/Node/2.3 NPM/index.js
@@ -0,0 +1,10 @@
+//var generateName = require("sillyname");
+// import generateName from "sillyname";
+// var sillyname = generateName();
+
+// console.log(`My name is ${sillyname}.`);
+
+ import {randomSuperhero} from 'superheroes';
+var superHero = randomSuperhero();
+
+console.log(`My name is ${superHero}!`);
